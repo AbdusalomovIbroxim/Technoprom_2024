a79ed9b0ddfcda2263b63026963958155d39beec
ibroxim <ibrohimabdusalomov395@gmail.com>
2024-03-12 17:19:35 +0500

    deploy

--- root/settings.py
+++ root/settings.py
@@ -164,7 +164,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
