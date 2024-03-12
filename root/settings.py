9b83ec24eb8d973729879824ca193be451e8b56f
ibroxim <ibrohimabdusalomov395@gmail.com>
2024-03-12 17:03:02 +0500

    deploy

--- root/settings.py
+++ root/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # settings.py
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "public")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
