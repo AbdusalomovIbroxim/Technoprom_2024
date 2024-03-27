1a0b21ad3c61169a660fa7171c7ef4e9596e4f81
ibroxim <ibrohimabdusalomov395@gmail.com>
2024-03-27 15:36:31 +0500

    deploy

--- event_calendar/urls.py
+++ event_calendar/urls.py
@@ -4,7 +4,7 @@ from .views import (EventLinkGenerator, DownloadICalendar,
                     )
 
 urlpatterns = [
-    path('', EventLinkGenerator.as_view(), name='index'),
+    path('', EventLinkGenerator.as_view(), name='link-pro'),
     path('save-links/', EventLinkGenerator.as_view(), name='save_links'),
     path('download-icalendar/<int:event_id>/', DownloadICalendar.as_view(), name='download_icalendar'),
     path('event-post/', PostEvent.as_view(), name='post_event'),
