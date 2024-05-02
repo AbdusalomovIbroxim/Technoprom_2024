00c43e339dc838310c8bbf8c910affd1f7af1649
ibroxim <ibrohimabdusalomov395@gmail.com>
2024-05-02 18:09:13 +0500

    commit

--- root/settings.py
+++ root/settings.py
@@ -147,7 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "ru"
+LANGUAGE_CODE = "uz"
 
 LANGUAGES = [
     ("en", _("English")),
